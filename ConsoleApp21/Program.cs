7bc1591bf7ec0f67691e8a88633d4af2866a2024
Катерина Буравлева <kkaatteerriinnaa2003@gmail.com>
2023-02-21 22:24:17 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp21/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    class Program
+    {
+        delegate void Delarr(int[] arr);
+        delegate void Text(string message);
+        static void Main(string[] args)
+        {
+            Random n = new Random();
+            string S = "String Int";
+            string s = "Int";
+            Delarr seven = (int[] arr) =>
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = n.Next(1,100);
+                    if (arr[i] % 7 == 0)
+                    {
+                        Console.Write(arr[i] + " ");
+                    }
+                }
+            };
+            Delarr possitive = (int[] arr) =>
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = n.Next(-100, 100);
+                    if (arr[i] > 0)
+                    {
+                        Console.Write(arr[i] + " ");
+                    }
+                }
+            };
+            Delarr negative = (int[] arr) =>
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = n.Next(-100, 1);
+                    if (arr[i] != arr[i+1])
+                    {
+                        Console.Write(arr[i] + " ");
+                    }
+                }
+            };
+            Text obj = delegate (string message)
+            {
+                if (s == S)
+                {
+                    Console.WriteLine("Есть слово " + s);
+                }
+            };
+        }
+    }
+}
